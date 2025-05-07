2de2cabaafac0e8c3cc799b8904013255f2cf2ec
victorrm3 <victoricomar3@gmail.com>
2025-05-07 16:30:21 +0200

    Configuracion vite para github

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/victorrm3.github.io/',  // Asegúrate de usar el nombre correcto de tu repositorio aquí
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,3 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
